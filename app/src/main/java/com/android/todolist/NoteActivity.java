460a518b3f173b45ce85055227f342824d479d68
blackdrozd <paint312@gmail.com>
2019-12-16 16:29:39 +0300

    added intent to NoteActivity

--- app/src/main/java/com/android/todolist/NoteActivity.java
+++ app/src/main/java/com/android/todolist/NoteActivity.java
@@ -1,15 +1,15 @@
 package com.android.todolist;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_new_note);
     }
 }
